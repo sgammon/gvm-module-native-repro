ffcf3407b8375523a62c619fff5c59d542d50939
Sam Gammon <sam@elide.dev>
2025-01-15 15:09:18 -0800

    fix: tweaks
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- NativeFfmFeature.java
+++ NativeFfmFeature.java
@@ -1,20 +1,15 @@
 package example;
 
 import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.Linker;
-import static java.lang.foreign.ValueLayout.*;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.Feature.DuringSetupAccess;
 import org.graalvm.nativeimage.hosted.RuntimeForeignAccess;
 
-class NativeFfmFeature implements Feature { 
+class NativeFfmFeature implements Feature {
+  @Override
   public void duringSetup(DuringSetupAccess access) {
-    RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.ofVoid());
-    RuntimeForeignAccess.registerForUpcall(FunctionDescriptor.ofVoid());
-    RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.ofVoid(), Linker.Option.critical(false));
-    RuntimeForeignAccess.registerForUpcall(FunctionDescriptor.of(JAVA_INT, JAVA_INT));
-    RuntimeForeignAccess.registerForUpcall(FunctionDescriptor.of(JAVA_INT, JAVA_INT, JAVA_INT));
-    RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_INT), Linker.Option.firstVariadicArg(1));
-    RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.ofVoid(JAVA_INT), Linker.Option.captureCallState("errno"));
+    RuntimeForeignAccess.registerForDowncall(
+      FunctionDescriptor.ofVoid()
+    );
   }
 }
